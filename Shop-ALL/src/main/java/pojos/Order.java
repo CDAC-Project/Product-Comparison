2c669e797a6102ead963bee7c4b9fe347a7af98f
Ajay Paratmandali <ajaypp123@gmail.com>
2018-06-08 15:14:40 +0530

    Pojos Updated

--- Shop-ALL/src/main/java/pojos/Order.java
+++ Shop-ALL/src/main/java/pojos/Order.java
@@ -11,6 +11,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -27,7 +28,7 @@ public class Order {
 	@Temporal(TemporalType.DATE)
 	Date orderedDate;
 	
-	@ManyToOne
+	@OneToOne
 	@JoinColumn(name="user_id")
 	User user;
 	
